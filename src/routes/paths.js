ac65a882e82fb37bb590ccb014a0a66496f0a5dc
Thimotey Tsan <thimotey.tsan@viveris.fr>
2021-09-06 10:19:52 +0200

    put better url path

--- src/routes/paths.js
+++ src/routes/paths.js
@@ -6,14 +6,14 @@ const PATHS = {
   },
   SEARCH: {
     GENE: '/search/genes',
-    GENE_ITEM: '/search/gene/:id',
-    ANATOMICAL_HOMOLOGY: '/search/anatomical-homologysss',
+    GENE_ITEM: '/search/genes/:id',
+    ANATOMICAL_HOMOLOGY: '/search/anatomical-homology',
     SPECIES: '/search/species',
     SPECIES_ITEM: '/search/species/:id',
   },
   DOWNLOAD: {
-    GENE_EXPRESSION_CALLS: '/download/gene-expr-calls',
-    PROCESSED_EXPRESSION_VALUES: '/download/proc-values',
+    GENE_EXPRESSION_CALLS: '/download/gene-expression-calls',
+    PROCESSED_EXPRESSION_VALUES: '/download/processed-expression-values',
     DATA_DUMPS: '/download/data-dumps',
   },
   RESOURCES: {
@@ -26,7 +26,7 @@ const PATHS = {
   SUPPORT: {
     GTEX: '/support/data-sets',
     TOP_ANAT: '/support/top-anat',
-    GENE_EXPRESSION_CALLS: '/support/call-files',
+    GENE_EXPRESSION_CALLS: '/support/gene-expression-calls',
     FAQ: '/support/faq',
   },
   ABOUT: {
