7a3f5b861775a366db7842dc6d0ffb550b0a78de
Thimotey Tsan <thimotey.tsan@viveris.fr>
2021-09-06 15:27:17 +0200

    instantiate reactive select

---
+++ src/components/Select/SelectCustom.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SelectCustom = ({ mutliple = false }) => {
+  const [selected, setSelected] = React.useState(null);
+
+  return <div className="select">test</div>;
+};
+
+export default SelectCustom;
