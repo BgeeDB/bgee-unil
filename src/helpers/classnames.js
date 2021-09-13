66536d6adb9a66178aa5df617bbb1f33bbc87b3c
Thimotey Tsan <thimotey.tsan@viveris.fr>
2021-09-13 14:59:44 +0200

    classnames  utility

---
+++ src/helpers/classnames.js
@@ -0,0 +1,39 @@
+const hasOwn = {}.hasOwnProperty;
+const classnames = (...args) => {
+  const classes = [];
+
+  for (let i = 0; i < args.length; i += 1) {
+    const arg = args[i];
+    // eslint-disable-next-line no-continue
+    if (!arg) continue;
+
+    const argType = typeof arg;
+
+    if (argType === 'string' || argType === 'number') {
+      classes.push(arg);
+    } else if (Array.isArray(arg)) {
+      if (arg.length) {
+        // eslint-disable-next-line prefer-spread
+        const inner = classnames.apply(null, arg);
+        if (inner) {
+          classes.push(inner);
+        }
+      }
+    } else if (argType === 'object') {
+      if (arg.toString === Object.prototype.toString) {
+        // eslint-disable-next-line no-restricted-syntax
+        for (const key in arg) {
+          if (hasOwn.call(arg, key) && arg[key]) {
+            classes.push(key);
+          }
+        }
+      } else {
+        classes.push(arg.toString());
+      }
+    }
+  }
+
+  return classes.join(' ');
+};
+
+export default classnames;
