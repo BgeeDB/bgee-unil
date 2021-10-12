0f5683b05190653c47df3725769b27fd96f240e9
Thimotey Tsan <thimotey.tsan@viveris.fr>
2021-10-12 11:38:55 +0200

    clean

--- src/components/Bulma/Modal/components/Card/ModalCardHeader.js
+++ src/components/Bulma/Modal/components/Card/ModalCardHeader.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Element from '../../../Element/Element';
 import classnames from '../../../../../helpers/classnames';
-import Button from '../../../Button/Button';
 
 const ModalCardHeader = ({ children, className, showClose, ...props }) => (
   <Element {...props} className={classnames('modal-card-head', className)}>
     {children}
-    {/* {showClose && <Button remove onClick={onClose} />} */}
   </Element>
 );
 
