daf0a16aeff0b87fe295d77079363eb263d79e50
Thimotey Tsan <thimotey.tsan@viveris.fr>
2021-11-05 14:37:47 +0100

    BGEE-190

--- src/components/Modal/DlProcessedExpressionValuesSpeciesModal.js
+++ src/components/Modal/DlProcessedExpressionValuesSpeciesModal.js
@@ -53,36 +53,44 @@ const DlProcessedExpressionValuesSpeciesModal = ({ species, files }) => {
           <p className="mb-2">
             <b>RNA-Seq data</b>
           </p>
-          <div className="field has-addons">
-            <p className="control">
-              <GaEvent
-                category="Processed Expression Values"
-                action="download_rna-seq_annotation-file"
-                label={files.rnaSeqAnnot.path}
-              >
-                <a className="button" href={files.rnaSeqAnnot.path}>
-                  <span className="is-size-7">
-                    Download experiments/libraries info
-                    {` (${readableFileSize(files.rnaSeqAnnot.size)})`}
-                  </span>
-                </a>
-              </GaEvent>
-            </p>
-            <p className="control">
-              <GaEvent
-                category="Processed Expression Values"
-                action="download_rna-seq_data-file"
-                label={files.rnaSeqData.path}
-              >
-                <a className="button" href={files.rnaSeqData.path}>
-                  <span className="is-size-7">
-                    Download read count, TPMs and FPKMs
-                    {` (${readableFileSize(files.rnaSeqData.size)})`}
-                  </span>
-                </a>
-              </GaEvent>
-            </p>
-          </div>
+          {files.rnaSeqAnnot || files.rnaSeqData ? (
+            <div className="field has-addons">
+              {files.rnaSeqAnnot && (
+                <p className="control">
+                  <GaEvent
+                    category="Processed Expression Values"
+                    action="download_rna-seq_annotation-file"
+                    label={files.rnaSeqAnnot.path}
+                  >
+                    <a className="button" href={files.rnaSeqAnnot.path}>
+                      <span className="is-size-7">
+                        Download experiments/libraries info
+                        {` (${readableFileSize(files.rnaSeqAnnot.size)})`}
+                      </span>
+                    </a>
+                  </GaEvent>
+                </p>
+              )}
+              {files.rnaSeqData && (
+                <p className="control">
+                  <GaEvent
+                    category="Processed Expression Values"
+                    action="download_rna-seq_data-file"
+                    label={files.rnaSeqData.path}
+                  >
+                    <a className="button" href={files.rnaSeqData.path}>
+                      <span className="is-size-7">
+                        Download read count, TPMs and FPKMs
+                        {` (${readableFileSize(files.rnaSeqData.size)})`}
+                      </span>
+                    </a>
+                  </GaEvent>
+                </p>
+              )}
+            </div>
+          ) : (
+            <p className="is-size-7 has-text-grey mb-2">No data</p>
+          )}
           <p className="is-size-7 has-text-grey">
             Files can also be retrieved per experiment, see{' '}
             <a
@@ -97,36 +105,44 @@ const DlProcessedExpressionValuesSpeciesModal = ({ species, files }) => {
           <p className="mb-2">
             <b>Affymetrix data</b>
           </p>
-          <div className="field has-addons">
-            <p className="control">
-              <GaEvent
-                category="Processed Expression Values"
-                action="download_affymetrix_annotation-file"
-                label={files.affymetrixAnnot.path}
-              >
-                <a className="button " href={files.affymetrixAnnot.path}>
-                  <span className="is-size-7">
-                    Download experiments/chips info
-                    {` (${readableFileSize(files.rnaSeqData.size)})`}
-                  </span>
-                </a>
-              </GaEvent>
-            </p>
-            <p className="control">
-              <GaEvent
-                category="Processed Expression Values"
-                action="download_affymetrix_data-file"
-                label={files.affymetrixData.path}
-              >
-                <a className="button t" href={files.affymetrixData.path}>
-                  <span className="is-size-7">
-                    Download signal intensities
-                    {` (${readableFileSize(files.affymetrixData.size)})`}
-                  </span>
-                </a>
-              </GaEvent>
-            </p>
-          </div>
+          {files.affymetrixAnnot || files.affymetrixData ? (
+            <div className="field has-addons">
+              {files.affymetrixAnnot && (
+                <p className="control">
+                  <GaEvent
+                    category="Processed Expression Values"
+                    action="download_affymetrix_annotation-file"
+                    label={files.affymetrixAnnot.path}
+                  >
+                    <a className="button " href={files.affymetrixAnnot.path}>
+                      <span className="is-size-7">
+                        Download experiments/chips info
+                        {` (${readableFileSize(files.rnaSeqData.size)})`}
+                      </span>
+                    </a>
+                  </GaEvent>
+                </p>
+              )}
+              {files.affymetrixData && (
+                <p className="control">
+                  <GaEvent
+                    category="Processed Expression Values"
+                    action="download_affymetrix_data-file"
+                    label={files.affymetrixData.path}
+                  >
+                    <a className="button t" href={files.affymetrixData.path}>
+                      <span className="is-size-7">
+                        Download signal intensities
+                        {` (${readableFileSize(files.affymetrixData.size)})`}
+                      </span>
+                    </a>
+                  </GaEvent>
+                </p>
+              )}
+            </div>
+          ) : (
+            <p className="is-size-7 has-text-grey mb-2">No data</p>
+          )}
           <p className="is-size-7 has-text-grey">
             Files can also be retrieved per experiment, see{' '}
             <a
@@ -143,34 +159,38 @@ const DlProcessedExpressionValuesSpeciesModal = ({ species, files }) => {
           </p>
           {files.fullLengthAnnot || files.fullLengthData ? (
             <div className="field has-addons">
-              <p className="control">
-                <GaEvent
-                  category="Processed Expression Values"
-                  action="download_full-length_annotation-file"
-                  label={files.fullLengthAnnot.path}
-                >
-                  <a className="button" href={files.fullLengthAnnot.path}>
-                    <span className="is-size-7">
-                      Download experiments/libraries info
-                      {` (${readableFileSize(files.fullLengthAnnot.size)})`}
-                    </span>
-                  </a>
-                </GaEvent>
-              </p>
-              <p className="control">
-                <GaEvent
-                  category="Processed Expression Values"
-                  action="download_full-length_data-file"
-                  label={files.fullLengthData.path}
-                >
-                  <a className="button" href={files.fullLengthData.path}>
-                    <span className="is-size-7">
-                      Download read count, TPMs and FPKMs
-                      {` (${readableFileSize(files.fullLengthData.size)})`}
-                    </span>
-                  </a>
-                </GaEvent>
-              </p>
+              {files.fullLengthAnnot && (
+                <p className="control">
+                  <GaEvent
+                    category="Processed Expression Values"
+                    action="download_full-length_annotation-file"
+                    label={files.fullLengthAnnot.path}
+                  >
+                    <a className="button" href={files.fullLengthAnnot.path}>
+                      <span className="is-size-7">
+                        Download experiments/libraries info
+                        {` (${readableFileSize(files.fullLengthAnnot.size)})`}
+                      </span>
+                    </a>
+                  </GaEvent>
+                </p>
+              )}
+              {files.fullLengthData && (
+                <p className="control">
+                  <GaEvent
+                    category="Processed Expression Values"
+                    action="download_full-length_data-file"
+                    label={files.fullLengthData.path}
+                  >
+                    <a className="button" href={files.fullLengthData.path}>
+                      <span className="is-size-7">
+                        Download read count, TPMs and FPKMs
+                        {` (${readableFileSize(files.fullLengthData.size)})`}
+                      </span>
+                    </a>
+                  </GaEvent>
+                </p>
+              )}
             </div>
           ) : (
             <p className="is-size-7 has-text-grey mb-2">No data</p>
