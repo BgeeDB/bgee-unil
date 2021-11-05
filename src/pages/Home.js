0209341b588aaf0a2576ef058a3c031291f331d2
Thimotey Tsan <thimotey.tsan@viveris.fr>
2021-11-05 16:52:57 +0100

    spacing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -85,117 +85,108 @@ const Home = () => {
         </Bulma.Hero.Body>
       </Bulma.Hero>
       <Bulma.Section>
-        <Bulma.Container>
-          <Bulma.Columns>
-            <Bulma.C size={12}>
-              <p className="has-text-centered">
-                {i18n.t('home.description-1')}
+        <Bulma.Columns>
+          <Bulma.C size={12}>
+            <p className="has-text-centered">{i18n.t('home.description-1')}</p>
+            <p className="has-text-centered">{i18n.t('home.description-2')}</p>
+          </Bulma.C>
+        </Bulma.Columns>
+        <Bulma.Columns>
+          <Bulma.C size={4}>
+            <div>
+              <p className="is-size-5">GENE EXPRESSION DATA</p>
+              Bgee is a database for retrieval and comparison of gene expression
+              patterns across multiple animal species, produced from multiple
+              data types (bulk RNA-Seq, single-cell RNA-Seq, Affymetrix, in situ
+              hybridization, and EST data) and from multiple data sets
+              (including{' '}
+              <LinkExternal to="https://www.gtexportal.org/home/">
+                GTEx data
+              </LinkExternal>
+              ).
+            </div>
+          </Bulma.C>
+          <Bulma.C size={4}>
+            <div>
+              <p className="is-size-5">SIMPLY NORMAL</p>
+              {/* eslint-disable-next-line react/no-unescaped-entities */}
+              Bgee is based exclusively on curated "normal", healthy wild-type,
+              expression data (e.g., no gene knock-out, no treatment, no
+              disease), to provide a comparable reference of normal gene
+              expression.
+            </div>
+          </Bulma.C>
+          <Bulma.C size={4}>
+            <div>
+              <p className="is-size-5">COMPARABLE BETWEEN SPECIES</p>
+              Bgee produces calls of presence/absence of expression, and of
+              differential over-/under-expression, integrated along with
+              information of gene orthology, and of homology between organs.
+              This allows comparisons of expression patterns between species.
+            </div>
+          </Bulma.C>
+        </Bulma.Columns>
+        <Bulma.Card className="mt-4">
+          <Bulma.Card.Header>
+            <Bulma.Card.Header.Title className="is-size-5 has-text-primary">
+              Species with data in Bgee
+            </Bulma.Card.Header.Title>
+          </Bulma.Card.Header>
+          <Bulma.Card.Body className="species">
+            <div className="content">
+              <div className="grid-species">
+                {speciesList.map((s) => (
+                  <div onClick={openSpeciesModal(s)} className="center-in-grid">
+                    <CardSpecies {...s} />
+                  </div>
+                ))}
+              </div>
+            </div>
+          </Bulma.Card.Body>
+        </Bulma.Card>
+        <Bulma.Card className="mt-4">
+          <HomeNewsList />
+        </Bulma.Card>
+        <div className="my-6 field is-grouped is-justify-content-center">
+          <p className="control">
+            <Link
+              className="button is-primary"
+              to={PATHS.ANALYSIS.EXPRESSION_COMPARISON}
+            >
+              <Bulma.IonIcon name="list-outline" />
+              <span>{i18n.t('home.exp-comparison')}</span>
+            </Link>
+          </p>
+          <p className="control">
+            <Link className="button is-primary" to={PATHS.ANALYSIS.TOP_ANAT}>
+              <Bulma.IonIcon name="stats-chart-outline" />
+              <span>{i18n.t('home.exp-enrichment-analysis')}</span>
+            </Link>
+          </p>
+          <p className="control">
+            <Link className="button is-primary" to={PATHS.SEARCH.GENE}>
+              <Bulma.IonIcon name="search-outline" />
+              <span>{i18n.t('home.gene-search')}</span>
+            </Link>
+          </p>
+        </div>
+        <Bulma.Columns>
+          <Bulma.C size={config.archive ? 12 : 9}>
+            <CreativeCommons />
+          </Bulma.C>
+          {!config.archive && (
+            <Bulma.C size={3}>
+              <p className="is-size-7 archived-link">
+                View archive sites:
+                {config.archivedVersion.map((archived) => (
+                  <LinkExternal key={archived.version} to={archived.url}>
+                    {`version ${archived.version}`}
+                  </LinkExternal>
+                ))}
               </p>
-              <p className="has-text-centered">
-                {i18n.t('home.description-2')}
-              </p>
             </Bulma.C>
-          </Bulma.Columns>
-          <Bulma.Columns>
-            <Bulma.C size={4}>
-              <div>
-                <p className="is-size-5">GENE EXPRESSION DATA</p>
-                Bgee is a database for retrieval and comparison of gene
-                expression patterns across multiple animal species, produced
-                from multiple data types (bulk RNA-Seq, single-cell RNA-Seq,
-                Affymetrix, in situ hybridization, and EST data) and from
-                multiple data sets (including{' '}
-                <LinkExternal to="https://www.gtexportal.org/home/">
-                  GTEx data
-                </LinkExternal>
-                ).
-              </div>
-            </Bulma.C>
-            <Bulma.C size={4}>
-              <div>
-                <p className="is-size-5">SIMPLY NORMAL</p>
-                {/* eslint-disable-next-line react/no-unescaped-entities */}
-                Bgee is based exclusively on curated "normal", healthy
-                wild-type, expression data (e.g., no gene knock-out, no
-                treatment, no disease), to provide a comparable reference of
-                normal gene expression.
-              </div>
-            </Bulma.C>
-            <Bulma.C size={4}>
-              <div>
-                <p className="is-size-5">COMPARABLE BETWEEN SPECIES</p>
-                Bgee produces calls of presence/absence of expression, and of
-                differential over-/under-expression, integrated along with
-                information of gene orthology, and of homology between organs.
-                This allows comparisons of expression patterns between species.
-              </div>
-            </Bulma.C>
-          </Bulma.Columns>
-          <Bulma.Card className="mt-4">
-            <Bulma.Card.Header>
-              <Bulma.Card.Header.Title className="is-size-5 has-text-primary">
-                Species with data in Bgee
-              </Bulma.Card.Header.Title>
-            </Bulma.Card.Header>
-            <Bulma.Card.Body className="species">
-              <div className="content">
-                <div className="grid-species">
-                  {speciesList.map((s) => (
-                    <div
-                      onClick={openSpeciesModal(s)}
-                      className="center-in-grid"
-                    >
-                      <CardSpecies {...s} />
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </Bulma.Card.Body>
-          </Bulma.Card>
-          <Bulma.Card className="mt-4">
-            <HomeNewsList />
-          </Bulma.Card>
-          <div className="my-6 field is-grouped is-justify-content-center">
-            <p className="control">
-              <Link
-                className="button is-primary"
-                to={PATHS.ANALYSIS.EXPRESSION_COMPARISON}
-              >
-                <Bulma.IonIcon name="list-outline" />
-                <span>{i18n.t('home.exp-comparison')}</span>
-              </Link>
-            </p>
-            <p className="control">
-              <Link className="button is-primary" to={PATHS.ANALYSIS.TOP_ANAT}>
-                <Bulma.IonIcon name="stats-chart-outline" />
-                <span>{i18n.t('home.exp-enrichment-analysis')}</span>
-              </Link>
-            </p>
-            <p className="control">
-              <Link className="button is-primary" to={PATHS.SEARCH.GENE}>
-                <Bulma.IonIcon name="search-outline" />
-                <span>{i18n.t('home.gene-search')}</span>
-              </Link>
-            </p>
-          </div>
-          <Bulma.Columns>
-            <Bulma.C size={config.archive ? 12 : 9}>
-              <CreativeCommons />
-            </Bulma.C>
-            {!config.archive && (
-              <Bulma.C size={3}>
-                <p className="is-size-7 archived-link">
-                  View archive sites:
-                  {config.archivedVersion.map((archived) => (
-                    <LinkExternal key={archived.version} to={archived.url}>
-                      {`version ${archived.version}`}
-                    </LinkExternal>
-                  ))}
-                </p>
-              </Bulma.C>
-            )}
-          </Bulma.Columns>
-        </Bulma.Container>
+          )}
+        </Bulma.Columns>
       </Bulma.Section>
     </>
   );
