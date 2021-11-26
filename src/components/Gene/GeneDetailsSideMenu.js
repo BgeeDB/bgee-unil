509f1d89330151bbe6c45c4ad89e9956bc0e976b
Thimotey Tsan <thimotey.tsan@viveris.fr>
2021-11-26 09:12:47 +0100

    fix 210 & 217

--- src/components/Gene/GeneDetailsSideMenu.js
+++ src/components/Gene/GeneDetailsSideMenu.js
@@ -3,13 +3,7 @@ import React from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 import GENE_DETAILS_HTML_IDS from '../../helpers/constants/GeneDetailsHtmlIds';
 
-const Styles = {
-  ulWidth: {
-    width: '57%',
-  },
-};
-
-const GeneDetailsSideMenu = () => {
+const GeneDetailsSideMenu = ({ homologs = null, isExpression, xRefs }) => {
   const history = useHistory();
   const location = useLocation();
 
@@ -22,10 +16,49 @@ const GeneDetailsSideMenu = () => {
 
   return (
     <aside className="menu">
-      <ul className="menu-list" style={Styles.ulWidth}>
-        {sideMenuElem.map((elem) => (
-          <li key={elem.domId} onClick={() => handlerMenuClick(elem.domId)}>
-            <a className="is-size-5 has-text-weight-semibold">{elem.name}</a>
+      <ul className="menu-list gene-menu">
+        <li
+          key={GENE_DETAILS_HTML_IDS.GENERAL_INFORMATION}
+          onClick={() =>
+            handlerMenuClick(GENE_DETAILS_HTML_IDS.GENERAL_INFORMATION)
+          }
+        >
+          <a className="is-size-5 has-text-weight-semibold">
+            General information
+          </a>
+        </li>
+        {isExpression && (
+          <li
+            key={GENE_DETAILS_HTML_IDS.EXPRESSION}
+            onClick={() => handlerMenuClick(GENE_DETAILS_HTML_IDS.EXPRESSION)}
+          >
+            <a className="is-size-5 has-text-weight-semibold">Expression</a>
+          </li>
+        )}
+        {homologs?.orthologs > 0 && (
+          <li
+            key={GENE_DETAILS_HTML_IDS.ORTHOLOGS}
+            onClick={() => handlerMenuClick(GENE_DETAILS_HTML_IDS.ORTHOLOGS)}
+          >
+            <a className="is-size-5 has-text-weight-semibold">Orthologs</a>
+          </li>
+        )}
+        {homologs?.paralogs > 0 && (
+          <li
+            key={GENE_DETAILS_HTML_IDS.PARALOGS}
+            onClick={() => handlerMenuClick(GENE_DETAILS_HTML_IDS.PARALOGS)}
+          >
+            <a className="is-size-5 has-text-weight-semibold">Paralogs</a>
+          </li>
+        )}
+        {xRefs && (
+          <li
+            key={GENE_DETAILS_HTML_IDS.XREFS}
+            onClick={() => handlerMenuClick(GENE_DETAILS_HTML_IDS.XREFS)}
+          >
+            <a className="is-size-5 has-text-weight-semibold">
+              Cross-references
+            </a>
           </li>
         )}
       </ul>
