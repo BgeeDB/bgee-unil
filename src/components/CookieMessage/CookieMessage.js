ebbf70dabe0c2aca68415d9d96ac8898feeb78cb
Sébastien Moretti <smoretti@unil.ch>
2022-01-17 15:13:02 +0100

    Fix text contrast + Fix CSS compatibility errors/warnings

--- src/components/CookieMessage/CookieMessage.js
+++ src/components/CookieMessage/CookieMessage.js
@@ -42,7 +42,7 @@ const CookieMessage = () => {
           This website requires cookies, and limited processing of your personal
           data in order to function. By using the site you are agreeing to this
           as outlined in our{' '}
-          <Link to={PATHS.ABOUT.PRIVACY_POLICY} className=" is-underlined">
+          <Link to={PATHS.ABOUT.PRIVACY_POLICY} className=" is-underlined" style="color:#FFF">
             privacy notice
           </Link>
           .
@@ -50,7 +50,7 @@ const CookieMessage = () => {
       </div>
       <div className="right">
         {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
-        <a className="is-underlined" onClick={acceptMessage}>
+        <a className="is-underlined" style="color:#FFF" onClick={acceptMessage}>
           Do not show this banner again
         </a>
       </div>
