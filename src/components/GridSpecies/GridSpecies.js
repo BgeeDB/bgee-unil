cc410baaa6afbe2229ffef8ffbbfbcd5746a899e
Tolotra Andrianalimanana <tolotra.andrianalimanana@externe.viveris.fr>
2022-10-12 15:32:50 +0300

    🔍 [BGEE-249] Add links to species list

--- src/components/GridSpecies/GridSpecies.js
+++ src/components/GridSpecies/GridSpecies.js
@@ -1,21 +1,61 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */
 import React from 'react';
+import { Link } from 'react-router-dom';
 import classnames from '../../helpers/classnames';
 import { CardSpecies } from '../CustomCard';
 import useQuery from '../../hooks/useQuery';
+
+const Wrapper = ({
+  species,
+  selectedSpecies,
+  setSelectedSpecies,
+  onClick,
+  onRenderSelection,
+  to,
+  children,
+}) => {
+  if (to) {
+    return <Link to={to(species)}>{children}</Link>;
+  }
+
+  return (
+    <div
+      id={`species-${species.id}`}
+      onClick={(e) => {
+        e.nativeEvent.preventDefault();
+        if (onClick) onClick(species, selectedSpecies !== species.id);
+        if (onRenderSelection && setSelectedSpecies)
+          setSelectedSpecies((prev) =>
+            prev === species.id ? undefined : species.id
+          );
+      }}
+      className={classnames(
+        'center-in-grid is-flex is-justify-content-center',
+        {
+          active: selectedSpecies === species.id,
+        }
+      )}
+    >
+      {children}
+    </div>
+  );
+};
 
 const GridSpecies = ({
   speciesList,
   onRenderSelection,
   onClick,
+  to,
   expandable,
   defaultSelection,
   scrollAt = true,
 }) => {
   const speciesID = useQuery('id');
+
   const [selectedSpecies, setSelectedSpecies] = React.useState(
     defaultSelection ? parseInt(defaultSelection, 10) : undefined
   );
+
   React.useEffect(() => {
     if (speciesID && scrollAt) {
       setSelectedSpecies(parseInt(speciesID, 10));
@@ -31,25 +71,17 @@ const GridSpecies = ({
     <div className="species-grid">
       {speciesList.map((species) => (
         <React.Fragment key={species.id}>
-          <div
-            id={`species-${species.id}`}
-            onClick={(e) => {
-              e.nativeEvent.preventDefault();
-              if (onClick) onClick(species, selectedSpecies !== species.id);
-              if (onRenderSelection)
-                setSelectedSpecies((prev) =>
-                  prev === species.id ? undefined : species.id
-                );
-            }}
-            className={classnames(
-              'center-in-grid is-flex is-justify-content-center',
-              {
-                active: selectedSpecies === species.id,
-              }
-            )}
+          <Wrapper
+            to={to}
+            species={species}
+            selectedSpecies={selectedSpecies}
+            setSelectedSpecies={setSelectedSpecies}
+            onClick={onClick}
+            onRenderSelection={onRenderSelection}
           >
             <CardSpecies {...species} />
-          </div>
+          </Wrapper>
+
           {!expandable &&
             onRenderSelection &&
             selectedSpecies === species.id &&
--- src/pages/search/SpeciesList.js
+++ src/pages/search/SpeciesList.js
@@ -1,14 +1,12 @@
 /* eslint-disable react/no-array-index-key */
 import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
-import { useHistory } from 'react-router-dom';
 import PATHS from '../../routes/paths';
 import Bulma from '../../components/Bulma';
 import api from '../../api';
 import GridSpecies from '../../components/GridSpecies/GridSpecies';
 
 const SpeciesList = () => {
-  const history = useHistory();
   const [speciesList, setSpeciesList] = useState([]);
 
   React.useEffect(() => {
@@ -48,8 +46,8 @@ const SpeciesList = () => {
         <div className="grid-species">
           <GridSpecies
             speciesList={speciesList}
-            onClick={(species) =>
-              history.push(PATHS.SEARCH.SPECIES_ITEM.replace(':id', species.id))
+            to={(species) =>
+              PATHS.SEARCH.SPECIES_ITEM.replace(':id', species.id)
             }
           />
         </div>
