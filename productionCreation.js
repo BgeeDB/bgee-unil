eae0962aebcd366a9a583e4fc41ee23ddf3dc31f
Sébastien Moretti <smoretti@unil.ch>
2023-09-26 13:43:10 +0200

    Generate the annotations.bgee.org build based on the default src/config.json

--- productionCreation.js
+++ productionCreation.js
@@ -2,11 +2,30 @@
 const fs = require('fs/promises');
 const fsStd = require('fs');
 const { execSync } = require('child_process');
+const ANNOT_URL = 'annotations.bgee.org';
 
 const main = async () => {
   try {
     let config = await fs.readFile('./src/config.json', 'utf8');
     config = JSON.parse(config);
+    const originalGenericDomain = config.genericDomain;
+    const originalPermanentVersionedDomain = config.permanentVersionedDomain;
+    const originalApiDomain = config.apiDomain;
+    const originalFtpDomain = config.ftpDomain;
+    if (config.isRawDataOnly) {
+      /* Change root URLs */
+      config.genericDomain = config.genericDomain.replace('www.bgee.org', `${ANNOT_URL}`);
+      config.permanentVersionedDomain = config.permanentVersionedDomain.replace('www.bgee.org', `${ANNOT_URL}`);
+      config.apiDomain = config.apiDomain.replace('www.bgee.org', `${ANNOT_URL}`);
+      config.ftpDomain = config.ftpDomain.replace('www.bgee.org', `${ANNOT_URL}`);
+      /* Change API URL */
+      const API_VERSION = config.version;
+      const API_DOMAIN = API_VERSION.replaceAll('.', '-');
+      config.apiDomain = config.apiDomain.replace('/api', `/api-${API_DOMAIN}`);
+      /* Write modified config.json */
+      await fs.writeFile('./src/config.json', JSON.stringify(config, null, 2));
+      console.log('Setting config as annotations');
+    }
 
     const scss = await fs.readFile('./src/styles/global.scss', 'utf8');
 
@@ -54,9 +73,25 @@ const main = async () => {
       console.log('Problem with prod environement : Canceling build');
       return;
     }
+    const paths = await fs.readFile('./src/routes/paths.js', 'utf8');
+    if (config.isRawDataOnly) {
+      console.log('\x1b[31m%s\x1b[0m', 'Annotation/Raw data interface settings');
+      /* Change the default homepage for the Annotation/Raw data interface */
+      const pathsTmp = paths.replace('HOME: `${URL_ROOT}/`', 'HOME: `${URL_ROOT}/search/raw-data`');
+      await fs.writeFile('./src/routes/paths.js', pathsTmp);
+    }
 
     console.log('\x1b[31m%s\x1b[0m', 'Building app');
     execSync('yarn build:cra', { stdio: 'inherit' });
+    if (config.isRawDataOnly) {
+      /* Back to default */
+      await fs.writeFile('./src/routes/paths.js', paths);
+      config.genericDomain = originalGenericDomain;
+      config.permanentVersionedDomain = originalPermanentVersionedDomain;
+      config.apiDomain = originalApiDomain;
+      config.ftpDomain = originalFtpDomain;
+      await fs.writeFile('./src/config.json', JSON.stringify(config, null, 2));
+    }
     console.log('\x1b[31m%s\x1b[0m\n', 'App built successfully');
   } catch (err) {
     console.log('\x1b[31m%s\x1b[0m', 'CATCH ! Error occured when building');
