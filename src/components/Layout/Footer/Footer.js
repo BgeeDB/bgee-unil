4dd57dfe3060f191d7ad4a9c006b918cf8f3390a
ndrs81 <andries81@gmail.com>
2024-05-16 17:38:05 +0200

    title change footer col

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -105,7 +105,7 @@ const Footer = () => {
           </div>
           <div className="nav-footer-col-spacer"/>
           <div className="nav-footer-col">
-            <div className="nav-footer-col-head">Share</div>
+            <div className="nav-footer-col-head">Share &amp; Social</div>
             <ul>
               <li>
                 <a
