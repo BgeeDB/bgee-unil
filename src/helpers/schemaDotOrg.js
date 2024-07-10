27d879b8a7ba967f6b513b7dd25850f461bbf1e3
Sébastien Moretti <smoretti@unil.ch>
2024-07-10 12:10:23 +0200

    Fixed the creator field in schema.org for Google dataset

--- src/helpers/schemaDotOrg.js
+++ src/helpers/schemaDotOrg.js
@@ -264,7 +264,12 @@ const datasetToLdJSON = () => {
             name: 'Bgee gene expression data',
             description: 'Bgee is a database for retrieval and comparison of gene expression patterns across multiple animal species. It provides an intuitive answer to the question -where is a gene expressed?- and supports research in cancer and agriculture, as well as evolutionary biology.',
             keywords: ['bgee', 'gene expression', 'evolution', 'ontology', 'anatomy', 'development', 'evo-devo database', 'anatomical ontology', 'developmental ontology', 'gene expression evolution'],
-            creator: {'@id': 'https://www.bgee.org/'},
+            creator: {
+                '@id': 'https://www.bgee.org/',
+                '@type': 'Organization',
+                url: 'https://www.bgee.org/',
+                name: 'The Bgee Team',
+            },
             citation: {
                 '@type': 'CreativeWork',
                 name: 'The Bgee suite: integrated curated expression atlas and comparative transcriptomics in animals',
